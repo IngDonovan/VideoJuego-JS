6118617c4adb24b383bb30c277937a2f7f041ef7
Donovan Rojas <rojasdrm03@gmail.com>
2023-03-15 06:53:51 -0500

    canvas responsive part 2

--- game.js
+++ game.js
@@ -2,9 +2,36 @@ const canvas = document.querySelector('#game');//seleccionar
 //para acceder a los metodos de ese elemento
 const game = canvas.getContext('2d');//juego en 2d
 
-window.addEventListener('load',startGame);//apenas cargue el html, para evitar que nos genere problemas a futuro
-window.addEventListener('resize',startGame);//para tomar el elemento de cambio de pantalla
+let canvasSize;
+let elementsSize;
 
+window.addEventListener('load',setCanvasSize);//apenas cargue el html, para evitar que nos genere problemas a futuro
+window.addEventListener('resize',setCanvasSize);//para tomar el elemento de cambio de pantalla
+
+
+
+function setCanvasSize() {
+
+    if (window.innerHeight > window.innerWidth) {
+        canvasSize = window.innerWidth * 0.8;
+    }
+    else {
+        canvasSize = window.innerHeight * 0.8;
+    }
+    
+    canvas.setAttribute('Width', canvasSize);
+    canvas.setAttribute('height', canvasSize);
+    
+    //para hacer responsive, lo usamos dentro del canvas, pero hace efecto cuando se recarga
+    //windows.innerHeight
+    //windows.innerWidth
+    //no tener en cuenta el conenido del html sino de la ventana
+    
+    //como calcular el ancho y el alto del elemento apartir de ese ancho y alto de nuestro canvas, si tenemos 100px respect necesitamos cada medida de 10px
+    elementsSize = canvasSize / 10;
+
+    startGame();//para que no se borre cada vez que se cambie de pantalla
+}
 
 //primero que todo crear una funcion para encapsular el codigo que vamos a llamar
 //para inicializar
@@ -35,27 +62,4 @@ function startGame() {
         game.fillText(emojis['X'],elementsSize,elementsSize * i);//el elemento 0 queda por fuera por eso le damos 1 para poder tener los 10 dentro del canvas
         
     }
-}
-
-function setCanvasSize() {
-    //para el calculo del cuadrado
-    let canvasSize;
-
-    if (window.innerHeight > window.innerWidth) {
-        canvasSize = window.innerWidth * 0.8;
-    }
-    else {
-        canvasSize = window.innerHeight * 0.8;
-    }
-    
-    canvas.setAttribute('Width', canvasSize);
-    canvas.setAttribute('height', canvasSize);
-    
-    //para hacer responsive, lo usamos dentro del canvas, pero hace efecto cuando se recarga
-    //windows.innerHeight
-    //windows.innerWidth
-    //no tener en cuenta el conenido del html sino de la ventana
-    
-    //como calcular el ancho y el alto del elemento apartir de ese ancho y alto de nuestro canvas, si tenemos 100px respect necesitamos cada medida de 10px
-    const elementsSize = canvasSize / 10;
 }
